b42aebfcb74d33b90970cdd09c2deb767fd4fce2
Vesta-Nassone <vestanassone@gmail.com>
2022-01-21 19:04:58 +0200

    Add a comment.

--- Dashboard.jsx
+++ Dashboard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 export default function Dashboard() {
-	return (
+    return (
+        /* this component will hold the top two rows of InfoCards and the Graphs */
 		<div>
 			<h1>This is the dashboard component</h1>
 		</div>
