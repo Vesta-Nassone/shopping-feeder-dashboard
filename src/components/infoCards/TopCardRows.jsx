eac6b733b73a8727542bcfed7f9d7cd54c0767fb
Vesta-Nassone <vestanassone@gmail.com>
2022-01-22 17:10:42 +0200

    Add the remaining cards.

--- src/components/infoCards/TopCardRows.jsx
+++ src/components/infoCards/TopCardRows.jsx
@@ -16,6 +16,115 @@ export default function TopCardRows() {
 					</div>
 				</div>
 			</div>
+			<div className="col px-2">
+				<div className="info-box">
+					<span className="info-box-icon">
+						<i className="bi bi-exclamation-triangle" />
+					</span>
+					<div className="info-box-text">
+						<h6>5</h6>
+						<p>Stores with Product Errors</p>
+					</div>
+				</div>
+			</div>
+			<div className="col px-2">
+				<div
+					className="info-box"
+					style={{ backgroundColor: "rgb(139,195,74)" }}
+				>
+					<span
+						className="info-box-icon"
+						style={{ backgroundColor: "rgb(122,171,65)" }}
+					>
+						<i className="bi bi-graph-up-arrow" />
+					</span>
+					<div className="info-box-text">
+						<h6>15</h6>
+						<p>Total Referrals Today</p>
+					</div>
+				</div>
+			</div>
+			<div className="col px-2">
+				<div
+					className="info-box"
+					style={{ backgroundColor: "rgb(139,195,74)" }}
+				>
+					<span
+						className="info-box-icon"
+						style={{ backgroundColor: "rgb(122,171,65)" }}
+					>
+						<i className="bi bi-cart3" />
+					</span>
+					<div className="info-box-text">
+						<h6>500</h6>
+						<p>Total Sales Today</p>
+					</div>
+				</div>
+			</div>
+			<div className="col px-2">
+				<div className="info-box">
+					<span className="info-box-icon">
+						<i className="bi bi-plus" />
+					</span>
+					<div className="info-box-text center">
+						<p>Add New Merchant</p>
+					</div>
+				</div>
+			</div>
+			<div className="col px-2">
+				<div className="info-box">
+					<span className="info-box-icon">
+						<i className="bi bi-person-badge" />
+					</span>
+					<div className="info-box-text center">
+						<p>Reassign Merchant</p>
+					</div>
+				</div>
+			</div>
+			<div className="col px-2">
+				<div className="info-box">
+					<span className="info-box-icon">
+						<i className="bi bi-cart-plus" />
+					</span>
+					<div className="info-box-text center">
+						<p>Add New Store</p>
+					</div>
+				</div>
+			</div>
+			<div className="col px-2">
+				<div
+					className="info-box"
+					style={{ backgroundColor: "rgb(139,195,74)" }}
+				>
+					<span
+						className="info-box-icon"
+						style={{ backgroundColor: "rgb(122,171,65)" }}
+					>
+						<i className="bi bi-bar-chart-fill" />
+					</span>
+					<div className="info-box-text">
+						<h6>300</h6>
+						<p>Card 4</p>
+					</div>
+				</div>
+			</div>
+			<div className="col px-2">
+				<div
+					className="info-box"
+					style={{ backgroundColor: "rgb(139,195,74)" }}
+				>
+					<span
+						className="info-box-icon"
+						style={{ backgroundColor: "rgb(122,171,65)" }}
+					>
+						<i className="bi bi-bar-chart-fill" />
+					</span>
+					<div className="info-box-text">
+						<h6>400</h6>
+						<p>Card 5</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
