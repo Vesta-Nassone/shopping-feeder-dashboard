7ab7a6b1e4d3f7d3cf63f41f727e8c9da92519b3
Vesta-Nassone <vestanassone@gmail.com>
2022-01-22 16:11:57 +0200

    Adjust the cols to match the specified height and width.

--- src/components/infoCards/TopCardRows.jsx
+++ src/components/infoCards/TopCardRows.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import "./TopCardRows.css";
 
 export default function TopCardRows() {
-    return <div className="row">
-        <div className="col px-2">
-            Col 1
-        </div>
-    </div>;
+	return (
+		<div className="row">
+			<div className="col px-2">
+				<div className="info-box">
+					<span className="info-box-icon">
+						<i className="bi bi-shop" />
+					</span>
+					<div className="info-box-text">
+						<h6>40</h6>
+						<p>Stores</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
