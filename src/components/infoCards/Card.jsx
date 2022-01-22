598e5823988ea8af5df6b55c84269da63cf672b3
Vesta-Nassone <vestanassone@gmail.com>
2022-01-22 17:20:54 +0200

    Add the props via propTypes.

--- src/components/infoCards/Card.jsx
+++ src/components/infoCards/Card.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+
+const propTypes = {
+	className: PropTypes.string,
+	cardTitle: PropTypes.string,
+	children: PropTypes.node,
+};
 
 export const Card = () => {
-  return <div></div>;
+	return <div></div>;
 };
+Card.propTypes = propTypes
