1c59e99afc9308b9271b9bb85544e5723b909b1f
Vesta-Nassone <vestanassone@gmail.com>
2022-01-22 17:08:53 +0200

    Add some comments.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import TopCardRows from "../components/infoCards/TopCardRows";
 
 export default function Dashboard() {
+    /* This Dashboard component will hold the TopRowCards, Graphs and Table */
 	return (
 		<div>
 			<TopCardRows />
