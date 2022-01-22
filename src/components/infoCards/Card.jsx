8393c099bdd2dca9ad57a5442eb1d5c62e0456c6
Vesta-Nassone <vestanassone@gmail.com>
2022-01-22 17:23:50 +0200

    Pass the props through the Card component.

--- src/components/infoCards/Card.jsx
+++ src/components/infoCards/Card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// Define the Option and Required props
 const propTypes = {
 	className: PropTypes.string,
 	cardTitle: PropTypes.string,
 	children: PropTypes.node,
 };
 
-export const Card = () => {
-	return <div></div>;
+export const Card = ({ className = "", children, cardTitle }) => {
+	return <div className={`${className} ${"info-box"}`}>{children}</div>;
 };
-Card.propTypes = propTypes
+Card.propTypes = propTypes;
