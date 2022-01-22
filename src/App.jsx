f9b52178f533605dae11374fe228942f684f76bf
Vesta-Nassone <vestanassone@gmail.com>
2022-01-22 16:19:26 +0200

    Add a wrapper to App.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Dashboard from "./pages/Dashboard";
 function App() {
 	return (
 		/* The App component will hold the Dashboard, which contains the Cards, Tables and Graphs. */
-		<div className="App">
+		<div className="wrapper">
 			<Dashboard />
 		</div>
 	);
