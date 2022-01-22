a7a61a3e4243608aee1e01e1ad9d9f5926bde5a3
Vesta-Nassone <vestanassone@gmail.com>
2022-01-22 16:18:23 +0200

    Render the Dashboard component into App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Dashboard</h1>
-    </div>
-  );
+	return (
+		/* The App component will hold the Dashboard, which contains the Cards, Tables and Graphs. */
+		<div className="App">
+			<Dashboard />
+		</div>
+	);
 }
 
 export default App;
