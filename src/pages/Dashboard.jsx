ef197ce8d8472cc2c4034578b8f3f01716689bc4
Vesta-Nassone <vestanassone@gmail.com>
2022-01-22 16:14:48 +0200

    Render the TopRowCards in Dashboard.

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TopCardRows from "../components/infoCards/TopCardRows";
+
+export default function Dashboard() {
+	return (
+		<div>
+			<TopCardRows />
+		</div>
+	);
+}
