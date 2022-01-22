982ee6819e63f72ed48c2eae111cb46a07812dc0
Vesta-Nassone <vestanassone@gmail.com>
2022-01-22 17:11:55 +0200

    Add a margin to the wrapper.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Dashboard from "./pages/Dashboard";
 function App() {
 	return (
 		/* The App component will hold the Dashboard, which contains the Cards, Tables and Graphs. */
-		<div className="wrapper">
+		<div className="wrapper m-4">
 			<Dashboard />
 		</div>
 	);
