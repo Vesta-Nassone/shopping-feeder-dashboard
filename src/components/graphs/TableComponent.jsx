7d32f91ec99c502c4ada6dcfc8c3c58b76ef8d06
Vesta-Nassone <vestanassone@gmail.com>
2022-01-23 07:28:17 +0200

    Created a seperate Table Component.

---
+++ src/components/graphs/TableComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+import { tableData } from "../../data/dummyData";
+
+export const TableComponent = () => {
+	return (
+		
+	);
+};
