92754eb46eadfa51ceaa09fc9a82c83a7a9ced00
Vesta-Nassone <vestanassone@gmail.com>
2022-01-23 08:22:30 +0200

    Add the dummy data for Least performing stores.

--- src/components/graphs/Graphs.jsx
+++ src/components/graphs/Graphs.jsx
@@ -70,6 +70,65 @@ const bestPerformingStoresData = [
 	},
 ];
 
+const leastPerformingStoresData = [
+	{
+		name: "Date A-B",
+		s1: 25,
+		s2: 40,
+		s3: 15,
+		s4: 41,
+		s5: 37,
+	},
+	{
+		name: "Date B-C",
+		s1: 15,
+		s2: 30,
+		s3: 25,
+		s4: 37,
+		s5: 37,
+	},
+	{
+		name: "Date C-D",
+		s1: 99,
+		s2: 22,
+		s3: 10,
+		s4: 80,
+		s5: 84,
+	},
+	{
+		name: "Date D-E",
+		s1: 38,
+		s2: 25,
+		s3: 30,
+		s4: 40,
+		s5: 79,
+	},
+	{
+		name: "Date E-F",
+		s1: 35,
+		s2: 22,
+		s3: 55,
+		s4: 76,
+		s5: 60,
+	},
+	{
+		name: "Date F-G",
+		s1: 62,
+		s2: 24,
+		s3: 71,
+		s4: 36,
+		s5: 20,
+	},
+	{
+		name: "Date G-H",
+		s1: 40,
+		s2: 35,
+		s3: 51,
+		s4: 38,
+		s5: 49,
+	},
+];
+
 export default function Graphs() {
 	return (
 		<div className="row gx-4 gy-4">
