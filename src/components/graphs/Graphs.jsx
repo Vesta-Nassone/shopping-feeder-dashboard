867b9219213d351d53e46206b26c129b4269eea1
Vesta-Nassone <vestanassone@gmail.com>
2022-01-23 07:52:23 +0200

    Add the return on ad spent button.

--- src/components/graphs/Graphs.jsx
+++ src/components/graphs/Graphs.jsx
@@ -21,11 +21,16 @@ export default function Graphs() {
 				</div>
 			</div>
 			<div className="col-lg-6">
-				<div className="card-body d-flex justify-content-between">
+				<div className="card-body ">
 					<h5 className="card-title">
 						TOP 5 LEAST PERFORMING STORES
 					</h5>
-					
+					<button
+						type="button"
+						className="btn btn-sm btn-outline-secondary disabled"
+					>
+						Return on Ad Spent
+					</button>
 				</div>
 				<hr />
 			</div>
