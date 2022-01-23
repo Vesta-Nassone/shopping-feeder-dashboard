8a13509f6f26c7d7dfc08c21e06bc55346e797a4
Vesta-Nassone <vestanassone@gmail.com>
2022-01-23 08:28:53 +0200

    Add the Referrals by Merchants Graph.

--- src/components/graphs/Graphs.jsx
+++ src/components/graphs/Graphs.jsx
@@ -325,6 +325,39 @@ export default function Graphs() {
 						</h5>
 					</div>
 					<hr />
+					<ResponsiveContainer width="100%" aspect={4 / 1}>
+						<LineChart
+							width={700}
+							height={300}
+							data={referralsData}
+							margin={{
+								top: 5,
+								right: 30,
+								left: 20,
+								bottom: 5,
+							}}
+						>
+							<CartesianGrid strokeDasharray="3 3" />
+							<XAxis dataKey="name" />
+							<YAxis />
+							<Tooltip />
+							<Legend />
+							<Line
+								name="merchant_1"
+								type="monotone"
+								dataKey="m1"
+								stroke="#8884d8"
+								activeDot={{ r: 8 }}
+							/>
+							<Line
+								name="merchant_2"
+								type="monotone"
+								dataKey="m2"
+								stroke="#b1dec2"
+								activeDot={{ r: 8 }}
+							/>
+						</LineChart>
+					</ResponsiveContainer>
 				</div>
 			</div>
 			{/* End of Referrals Section */}
