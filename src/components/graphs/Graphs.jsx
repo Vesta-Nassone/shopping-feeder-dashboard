483a03be598d77f82485ba0d7019d7cb2f347a0e
Vesta-Nassone <vestanassone@gmail.com>
2022-01-23 07:01:36 +0200

    Make the able responsive.

--- src/components/graphs/Graphs.jsx
+++ src/components/graphs/Graphs.jsx
@@ -17,7 +17,7 @@ export default function Graphs() {
 					</div>
 					<hr />
 					<div className="p-2">
-					<table className="table  table-bordered table-hover">
+					<table className="table  table-bordered table-hover responsive">
 							<thead className="thead-dark">
 								<tr>
 									<th scope="col">Merchant</th>
