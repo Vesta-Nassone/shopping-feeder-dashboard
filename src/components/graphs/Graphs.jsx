b8ea756c8264adfe5613e5a30c4a2a71ece2ce25
Vesta-Nassone <vestanassone@gmail.com>
2022-01-23 07:48:59 +0200

    Add the top 5 perfoming card.

--- src/components/graphs/Graphs.jsx
+++ src/components/graphs/Graphs.jsx
@@ -4,7 +4,17 @@ import { TableComponent } from "./TableComponent";
 export default function Graphs() {
 	return (
 		<div className="row gx-4 gy-6">
-			<div className="col-lg-6">TOP 5 BEST PERFOMRING STORES</div>
+			<div className="col-lg-6">
+				<div className="card">
+					<div className="card-body d-flex justify-content-between">
+						<h5 className="card-title">
+							TOP 5 BEST PERFORMING STORES
+						</h5>
+	
+					</div>
+					<hr />
+				</div>
+			</div>
 			<div className="col-lg-6">TOP 5 LEAST PERFORMING</div>
 			<div className="col-lg-6">REFERRALS BY MERCHANT AND DATE</div>
 			{/* Start of the Table section.
