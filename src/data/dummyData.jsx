a1708a85432d621a321f55ccd5e3eae37734c770
Vesta-Nassone <vestanassone@gmail.com>
2022-01-23 06:29:24 +0200

    Populate the dummy table data.

---
+++ src/data/dummyData.jsx
@@ -0,0 +1,20 @@
+export const tableData = [
+	{
+		name: "Kevin",
+		store: "Woolworths",
+		channel: "Price Check",
+		product: "Milk",
+	},
+	{
+		name: "Genavieve",
+		store: "Shoprite",
+		channel: "Facebook",
+		product: "Bread",
+	},
+	{
+		name: "Vesta",
+		store: "Spar",
+		channel: "Google",
+		product: "Eggs",
+	},
+];
