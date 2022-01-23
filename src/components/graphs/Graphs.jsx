def0ccdcb98425e0085ba2e59d5a698749f8f111
Vesta-Nassone <vestanassone@gmail.com>
2022-01-23 06:38:43 +0200

    Add a bootstrap card around the table.

--- src/components/graphs/Graphs.jsx
+++ src/components/graphs/Graphs.jsx
@@ -7,7 +7,15 @@ export default function Graphs() {
 			<div className="col-lg-6">TOP 5 BEST PERFOMRING STORES</div>
 			<div className="col-lg-6">TOP 5 LEAST PERFORMING</div>
 			<div className="col-lg-6">REFERRALS BY MERCHANT AND DATE</div>
-			<div className="col-lg-6">TOP PRODUCTS BY MERCHANT AND DATE</div>
+			<div className="col-lg-6">
+				<div className="card">
+					<div className="card-body">
+						<h5 className="card-title">
+							TOP PRODUCTS BY MERCHANT AND DATE
+						</h5>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
