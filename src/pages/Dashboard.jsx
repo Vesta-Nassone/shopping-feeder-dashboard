e8413d6915defdc93ef25d292c9a0f7926aff9dc
Vesta-Nassone <vestanassone@gmail.com>
2022-01-24 03:02:46 +0200

    Wrap the CardProvider around the TopCardRows and Graphs. This will allow the props to be managed from a central place.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import TopCardRows from "../components/infoCards/TopCardRows";
 import Graphs from "../components/graphs/Graphs";
+import { CardProvider } from "../components/CardContext";
 
 export default function Dashboard() {
-    /* This Dashboard component will hold the TopRowCards, Graphs and Table */
+	/* 
+		This Dashboard component will hold the TopRowCards, Graphs and Table.
+		Wrap the CardProvider around the TopCardRows and Graph Components.
+		This will allow props to be passed down from the Parent to the Child components.
+		State will be managed from a central position.
+	 */
 	return (
-		<div>
+		<CardProvider>
 			<TopCardRows />
-			<Graphs/>
-		</div>
+			<Graphs />
+		</CardProvider>
 	);
 }
