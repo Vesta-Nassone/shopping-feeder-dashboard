824d20b3d0ebc077554dab36753c2e3ddc719c81
Vesta-Nassone <vestanassone@gmail.com>
2022-01-24 02:51:52 +0200

    Move the state of stores and merchants to CardContext.

--- src/components/CardContext.jsx
+++ src/components/CardContext.jsx
@@ -4,5 +4,8 @@ import React, { useState, createContext } from "react";
 export const CardContext = createContext();
 
 export const CardProvider = props => {
-	
+	const [merchants, setMerchants] = useState(2);
+	const [stores, setStores] = useState(5);
+
+	return <CardContext.Provider value={[merchants, setMerchants, stores, setStores]}>{props.children}</CardContext.Provider>;
 };
