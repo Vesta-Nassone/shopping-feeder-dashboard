4cb4289b5a0a2e6bd2d6128c8487a0c7d7e93710
Vesta-Nassone <vestanassone@gmail.com>
2022-01-24 02:54:09 +0200

    Add some comments.

--- src/components/CardContext.jsx
+++ src/components/CardContext.jsx
@@ -3,9 +3,19 @@ import React, { useState, createContext } from "react";
 // @ts-ignore
 export const CardContext = createContext();
 
-export const CardProvider = props => {
+/* The state of the stores and merchants will be managed here */
+export const CardProvider = (props) => {
 	const [merchants, setMerchants] = useState(2);
 	const [stores, setStores] = useState(5);
 
-	return <CardContext.Provider value={[merchants, setMerchants, stores, setStores]}>{props.children}</CardContext.Provider>;
+    return (
+        // Wrap the ContextProvider around any child element.
+        // This allows the props to be passed down easily.
+        // And state can be managed from one central point.
+		<CardContext.Provider
+			value={[merchants, setMerchants, stores, setStores]}
+		>
+			{props.children}
+		</CardContext.Provider>
+	);
 };
